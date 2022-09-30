5fb9ec934ab72462c66fd52545cfec792e20bdbd
MaxX <33282787+m1644@users.noreply.github.com>
2022-09-30 08:16:44 +0300

    fix DZ_001

--- Seminar_DZ/DZ_001.py
+++ Seminar_DZ/DZ_001.py
@@ -5,9 +5,23 @@
     - 0,56 -> 11
 '''
 
+'''
 num = input('Введите вещественное число: ')
 sum = 0
 for i in num:
     if i != ".":
         sum = sum + int(i)
 print(f'Сумма цифр числа {num} равна =', sum)
+'''
+
+def func(number):
+    sum = 0
+    for symbol in number:
+        if symbol != '.':
+            sum += int(symbol)
+    return sum
+
+if __name__ == '__main__':
+    chislo = input('Введите вещественное число: ')
+    summary = func(chislo)
+    print(f'Сумма цифр числа {chislo} равна =', summary)
