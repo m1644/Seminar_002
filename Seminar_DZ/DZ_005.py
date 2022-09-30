65acbdf6b4f0d239097e7fdb45d7278a340be18f
MaxX <33282787+m1644@users.noreply.github.com>
2022-09-30 09:15:12 +0300

    fix DZ_005

--- Seminar_DZ/DZ_005.py
+++ Seminar_DZ/DZ_005.py
@@ -2,6 +2,7 @@
 5. Реализуйте алгоритм перемешивания списка.
 '''
 
+'''
 list = ['Машина', 'Колесо', 'Руль', 'Педали']
 print(f'Исходный список: {list}')
 
@@ -10,3 +11,19 @@ def mix_list(list):
     random.shuffle(list)
     print(f'Перемешанный список: {list}')
 res = mix_list(list)
+'''
+
+from random import randint
+
+def func(N):
+    lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    res = []
+    for i in range(len(lst)):
+        index = randint(0, len(lst) - 1)
+        res.append(lst[index])
+        lst.remove(lst[index])
+    print(res)
+
+if __name__ == '__main__':
+    chislo = int(input('Введите число N: '))
+    func(chislo)
